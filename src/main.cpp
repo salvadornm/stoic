366d4d61829c37ee26f6e424f87b54a623f91910
Kate <kbuehner@alumni.stanford.edu>
2022-01-24 07:16:27 +0000

    particles moving

--- src/main.cpp
+++ src/main.cpp
@@ -30,8 +30,6 @@ int main(int argc, char* argv[])
     
     //** VARIABLES **//
     Cfd simulation;
-    //time step integration:
-    double dt = 0.01;
 
     //simulation parameters
     simulation.nparticles = 1000;
@@ -40,8 +38,7 @@ int main(int argc, char* argv[])
     simulation.frame = 10;
     simulation.delta = 0.01;
 
-    // implements a 1D std:vector like structure for doubles
-    // used to create grid   
+    // implements a 1D std:vector like structure to create grid   
     openfpm::vector<double> x;
     openfpm::vector<openfpm::vector<double>> y;
 
@@ -51,7 +48,7 @@ int main(int argc, char* argv[])
     openfpm_init(&argc,&argv);
 
     Box<3,double> domain({0.0,0.0,0.0},{1.0,1.0,1.0});  //define 3D box
-    size_t bc[3]={PERIODIC,PERIODIC,PERIODIC};    // boundary conditions
+    size_t bc[3]={PERIODIC,PERIODIC,PERIODIC};    // boundary conditions: Periodic means the 1 boundary is equal to the 0 boundary
     Ghost<3,double> ghost(simulation.delta);   // extended to contain interaction radius
     
     //randomly place particles within box
@@ -62,13 +59,17 @@ int main(int argc, char* argv[])
     vector_dist<3,double, aggregate<double[3],double[3]> > vd(0,domain,bc,ghost);
     //typedef vector_dist<3,double,aggregate<size_t,double,double,double,double,double[3],double[3], double[3]>> particles;
 
-    size_t cnt = 0; //used later
+    size_t cnt = 0; //used later    
+    
+    openfpm::vector<std::string> names({"velocity","force"});
+    vd.setPropNames(names);
 
     //**ASSIGN Random POSITION**//
     //should generate all values
     auto it = vd.getDomainIterator();
-    while (it.isNext())
+    for (size_t i = 0; i < simulation.nparticles ; i++)
     {
+        vd.add();
         auto key = it.get();    //contains (i,j,k) index of grid
 
         // we define x, assign a random position between 0.0 and 1.0
@@ -94,13 +95,27 @@ int main(int argc, char* argv[])
     vd.map();       //distribute particle positions across the processors       
     vd.ghost_get<>();   //syncs the ghost with the newly mapped particles
 
+    auto it2 = vd.getDomainIterator();
+    while (it2.isNext())
+    {
+        auto key = it2.get();    //contains (i,j,k) index of grid
+
+        //set the property of the particles
+        vd.template getProp<velocity>(key)[0] = 2.0;
+        vd.template getProp<velocity>(key)[1] = 1.0;
+        vd.template getProp<velocity>(key)[2] = 0.0;
+        
+        // next particle
+        ++it2;
+    }
+
     cout << " ---------  particles initialized  ------- " << cnt << endl;
  
-
     //**ASSIGNING VALUES**//
 
     timer tsim;
     tsim.start();
+    double dt = simulation.dt;
 
     auto NN = vd.getCellList<CELL_MEMBAL(3,double)>(simulation.delta); //cell list structure
 
@@ -118,9 +133,9 @@ int main(int argc, char* argv[])
 
             // v = v + .5dt calculate v(tn + 0.5)
             // velocity is always dependent on the previous velocity (getProp)
-            vd.template getProp<velocity>(p)[0] += 0.5*dt;
-            vd.template getProp<velocity>(p)[1] += 0.5*dt;
-            vd.template getProp<velocity>(p)[2] += 0.5*dt;
+            //vd.template getProp<velocity>(p)[0] += 0.5*dt;
+            //vd.template getProp<velocity>(p)[1] += 0.5*dt;
+            //vd.template getProp<velocity>(p)[2] += 0.5*dt;
 
             // calculate x(tn + 1)
             vd.getPos(p)[0] += vd.template getProp<velocity>(p)[0]*dt;
@@ -159,8 +174,7 @@ int main(int argc, char* argv[])
     //**VISUALIZATION**// <--- NEEDS UPDATED STILL
     //A VTK file contains information about particle position and properties
 
-    //openfpm::vector<std::string> names({"velocity","force"});
-    //vd.setPropNames(names);
+
     // save vtk format (vtk is always the default)
     //vd.write("particles_moving");
     // save in vtk format with time
