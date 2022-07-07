639eb37f8db242e7f038b4edec3bef78fc7898fc
Kate <kbuehner@alumni.stanford.edu>
2022-07-07 08:53:36 +0100

    kernel fixed.

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -17,18 +17,16 @@ double Wab(double r)
 // define gradient of cubic kernel function
 double DWab(Point<3,double> & dx, Point<3,double> & DW, double r, bool print)
 {
-    const double qq=r/H;
-    //const double qq = r;
-    double qq2 = qq * qq;
-    double fac1 = (c1*qq + d1*qq2)/r;
-    double b1 = (qq < 1.0)?1.0f:0.0f;
-    
-    double wqq = (2.0 - qq);
-    double fac2 = c2 * wqq * wqq/r;
-    double b2 = (qq >= 1.0 && qq < 2.0)?1.0f:0.0f;
-    
-    double factor = (b1*fac1 + b2*fac2);// /a2; //normalize (/a2/H)
-    
+    r /= H;
+    double factor = 0;
+    //std::cout << "r/h = " << r << std::endl;
+    if (r < 1.0)
+        factor = (c1*r + d1*r*r);  //*(a2/H));
+    else if (r < 2.0)
+        factor = (c2*(2.0 - r)*(2.0 - r));  //*(a2/H);
+    else
+        factor = 0.0;
+
     DW.get(0) = factor * dx.get(0);
     DW.get(1) = factor * dx.get(1);
     DW.get(2) = factor * dx.get(2);
